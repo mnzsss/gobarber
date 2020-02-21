22ae2e86f8b034257ea16b20eb482aadfa512a9f
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-02-21 12:15:29 -0300

    send mail config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '7cffabc77fb75d',
+    pass: '104f892f08ea91',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
