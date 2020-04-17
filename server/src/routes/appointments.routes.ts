3bd7dec29c1ce8d791907c913eb4ffb8eb2a3bd7
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-04-16 23:25:14 -0300

    feat: Repositories

---
+++ server/src/routes/appointments.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (req, res) => {
+  const appointments = appointmentsRepository.all();
+
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/', (req, res) => {
+  const { provider, date } = req.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return res
+      .status(400)
+      .json({ error: 'This appointment is already booked.' });
+  }
+
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
+
+  return res.json(appointment);
+});
+
+export default appointmentsRouter;
