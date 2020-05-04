245707a5b0c44f1c93a17e371d3513a62178d2d7
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-05-04 15:58:14 -0300

    feat: Global Exeption Error Handling

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return res
       .status(err.statusCode)
