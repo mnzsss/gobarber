fac3f7f9b8feca4e8e1bcc5216e663aeb0aae6bd
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-05-10 23:45:06 -0300

    🎉 initial commit

---
+++ appgobarber/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <View style={{ backgroundColor: '#312e38', flex: 1 }} />
+    </>
+  );
+};
+
+export default App;
