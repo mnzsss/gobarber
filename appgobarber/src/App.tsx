92267d1da554a7e1a903f9983537dc20f626a180
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-05-10 23:58:27 -0300

    feat: Routes

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,12 +1,16 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+
+import AuthRoutes from './routes';
 
 const App: React.FC = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-      <View style={{ backgroundColor: '#312e38', flex: 1 }} />
-    </>
+      <AuthRoutes />
+    </NavigationContainer>
   );
 };
 
