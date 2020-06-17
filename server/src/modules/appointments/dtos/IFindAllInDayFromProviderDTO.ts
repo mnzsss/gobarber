78838a3d54475108797cc977e9e64705d9890b26
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-06-17 19:26:38 -0300

    chore: return user to list appointments

--- server/src/modules/appointments/dtos/IFindAllInDayFromProviderDTO.ts
+++ server/src/modules/appointments/dtos/IFindAllInDayFromProviderDTO.ts
@@ -2,5 +2,5 @@ export default interface IFindAllInDayFromProviderDTO {
   provider_id: string;
   day: number;
   year: number;
-  month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  month: number;
 }
