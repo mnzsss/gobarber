f6bc444f8c8565ac97cff77f83a0fff685a9c59d
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-06-17 19:10:39 -0300

    fix: filter by date with provider

--- server/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ server/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -11,9 +11,12 @@ import Appointment from '../../infra/typeorm/entities/Appointment';
 class FakeAppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
-  public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(app =>
-      isEqual(app.date, date),
+  public async findByDate(
+    date: Date,
+    provider_id: string,
+  ): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      app => isEqual(app.date, date) && app.provider_id === provider_id,
     );
 
     return findAppointment;
