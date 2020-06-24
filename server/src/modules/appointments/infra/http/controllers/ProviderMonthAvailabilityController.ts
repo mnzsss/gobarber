68f6560fb6f7813bffba6076dc9bc7af8675affe
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-06-24 14:16:56 -0300

    fix: get filter with query Appointments Month

--- server/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ server/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -6,7 +6,7 @@ import ListProviderMonthAvailabilityService from '@modules/appointments/services
 class ProviderMonthAvailabilityController {
   async index(req: Request, res: Response): Promise<Response> {
     const { provider_id } = req.params;
-    const { month, year } = req.body;
+    const { month, year } = req.query;
 
     const listProviderMonthAvailability = container.resolve(
       ListProviderMonthAvailabilityService,
